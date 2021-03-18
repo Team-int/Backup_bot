7db5e990a6df270857ce9a4bdd6392ef48f81297
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2021-03-18 17:41:28 +0800

    포맷 데이터 번역됨

--- xenon/utils/formatter.py
+++ xenon/utils/formatter.py
@@ -3,24 +3,24 @@ import discord
 # https://www.iconfinder.com/iconsets/small-n-flat
 message_types = {
     None: ("", "{c}", "", 0x36393e),
-    "info": ("Info", "{c}",
+    "info": ("정보", "{c}",
              "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678110-sign-info-512.png", 0x478fce),
     "wait_for": ("Waiting for response", "{c}", "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/bubbles-alt2-512.png", 0x478fce),
-    "success": ("Voila!", "{c}", "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678134-sign-check-512.png",
+    "success": ("워후!", "{c}", "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678134-sign-check-512.png",
                 0x48ce6c),
-    "warning": ("Warning", "{c}",
+    "warning": ("경고", "{c}",
                 "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678136-shield-warning-512.png",
                 0xefbc2f),
-    "working": ("Please wait ...", "{c}",
+    "working": ("잠시만 기다려 주십시오...", "{c}",
                 "https://images-ext-1.discordapp.net/external/AzWR8HxPJ4t4rPA1DagxJkZsOCOMp4OTgwxL3QAjF4U/https/cdn.discordapp.com/emojis/424900448663633920.gif",
                 0x36393e),
-    "error": ("Error", "{c}" + f"\n\n[Support](https://discord.club/discord)",
+    "error": ("오류", "{c}" + f"\n\n[Support](https://discord.club/discord)",
               "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678069-sign-error-512.png",
               0xc64935),
-    "perm_error": ("Insufficient Permission", "{c}",
+    "perm_error": ("권한 부족", "{c}",
                    "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678069-sign-error-512.png",
                    0xc64935),
-    "unex_error": ("Error", "**Error Code:**\n```{c}```" + f"\n\n[Support](https://discord.club/discord)",
+    "unex_error": ("오류", "**오류 코드:**\n```{c}```" + f"\n\n[지원 서버](https://discord.club/discord)",
                    "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678069-sign-error-512.png", 0xc64935)
 }
 
